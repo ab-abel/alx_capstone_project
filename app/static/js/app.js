c209dfe1053e4fa6e8c7b045f1feb2d926bc3c8b
abel <ab.bell205@gmail.com>
2023-10-14 16:59:42 +0100

    added alert for popup

---
+++ app/static/js/app.js
@@ -0,0 +1,38 @@
+// get input element from field
+
+const select = document.getElementById('select-id')
+const alert = document.getElementById('alert')
+
+
+
+select.addEventListener('change', (e)=>{
+    e.preventDefault();
+    document.getElementById('alert').innerHTML = '';
+
+    if(select.value == '1'){
+        const span = document.createElement('span');
+        span.setAttribute('class', 'close-alert');
+        span.innerHTML = '&times;';
+       
+        const p = document.createElement('p');
+        p.style.margin = 0;
+        p.innerHTML = 'Hint: Enter 100-300 to search Calories between that range';
+        alert.appendChild(span);
+        alert.appendChild(p);
+        span.setAttribute('onclick',"this.parentElement.style.display='none'")
+        
+    }
+
+    else if(select.value == '2') {
+        const span = document.createElement('span');
+        span.setAttribute('class', 'close-alert');
+        span.innerHTML = '&times;';
+        span.setAttribute('onclick',"this.parentElement.style.display='none'")
+        const p = document.createElement('p');
+        p.innerHTML = 'Hint: Enter 1-3 to search ingredient between that range';
+        alert.appendChild(span);
+        alert.appendChild(p);
+    }
+})
+
+
