1b8d8a4919520a5c9d5e3925c96b8cd22dce5ef5
abel <ab.bell205@gmail.com>
2023-10-15 19:28:39 +0100

    modify validate

--- app/static/js/validate.js
+++ app/static/js/validate.js
@@ -1,33 +1,89 @@
 const search_btn = document.getElementById('search-btn');
-const search = document.getElementById('search').value
 
-const select_val = document.getElementById('select-id')
-var option_user_selection = select_val.options[ select_val.selectedIndex ].value
 
 search_btn.addEventListener('click', (e)=>{
+    document.getElementById('alert').innerHTML =''
     e.preventDefault();
+    const search = document.getElementById('search').value;
+    const select_val = document.getElementById('select-id').value;
+    const alert = document.getElementById('alert');
+
+    const alert_p = document.createElement('p');
+    // const option_user_selection = select_val.options[select_val.selectedIndex].text;
+
     //fnction call
-    console.log(option_user_selection)
+    // console.log(typeof(select_val))
+    
     let isValid = false;
-    if (select_val == '0' || select_val == '2') {
-        if (!isRequired(search) || !isNumeric(search)){
+    if (select_val == 0 ) {
+        if (!isNumeric(search)  && isRequired(search) && !isSpecialCharacters(search)){
             isValid = true;
+        }else{
+            alert_p.innerHTML = 'Search field must be a non Empty String';
+            alert_p.style.color = 'red';
+            alert_p.style.padding = '.5rem';
+            isValid = false;
         }
-        // console.log(select_val)
+        
     }
-    else if (select_val === '1' || select_val === '3') {
-        if(!isRequired(search) && isNumeric(search) && !isSpecialCharacters(search)) {
+    else if ( select_val == 2){
+        if (!isNumeric(search)  && isRequired(search) && !isSpecialCharacters(search)){
             isValid = true;
+        }else{
+            alert_p.innerHTML = 'Diet field must be a string and  not empty ';
+            alert_p.style.color = 'red';
+            alert_p.style.padding = '.5rem';
+            isValid = false;
         }
-
     }
-    else {
-        isValid = false;
+    else if (select_val == 1) {
+        if(isRequired(search) && isNumeric(search) && !isSpecialCharacters(search)) {
+            isValid = true;
+        }else{
+            alert_p.innerHTML = 'Calories Field must be an integer or a Range of value seprated by -';
+            alert_p.style.color = 'red';
+            alert_p.style.padding = '.5rem';
+            isValid = false;
+        }
+    } else if (select_val == 3){
+        if(isRequired(search) && isNumeric(search) && !isSpecialCharacters(search)) {
+            isValid = true;
+        }else{
+            alert_p.innerHTML = 'Ingredient Field must be an integer or a Range of value seprated by -';
+            alert_p.style.color = 'red';
+            alert_p.style.padding = '.5rem';
+            isValid = false;
+        }
     }
+    alert.appendChild(alert_p);
 
-    console.log(isValid);
- });
+    if(isValid){
 
+        var form_data = {
+            'search':search,
+            'selected_option':select_val
+        };
+        // send data to backend
+        fetch('/search', {
+            method:'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(form_data),
+            
+        })
+        .then(response=>{
+            
+            if(response.redirected){
+                // console.log(response.url[0])
+                window.location.href = response.url;
+            }else{
+                response.json();  
+            }
+         });
+ 
+        }
+});
 
 // helper functions
 const isRequired = value => value === '' ? false:true;
@@ -38,7 +94,7 @@ const isRequired = value => value === '' ? false:true;
  * @returns 
  */
 const isSpecialCharacters = (search) => {
-    const re = new RegExp("^(?=.*[!@#\$%_\^&\*])");
+    const re = new RegExp("^(?=.*[!@#\$%_+?,'`();:\^&\*])");
     return re.test(search);
 }
 
