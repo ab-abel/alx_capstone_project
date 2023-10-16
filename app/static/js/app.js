45db94c839c76e930840e119fa3c3854ccddf8b7
abel <ab.bell205@gmail.com>
2023-10-16 13:00:08 +0100

    added cart feature

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -35,4 +35,92 @@ select.addEventListener('change', (e)=>{
     }
 })
 
+// cart
+
+const cart = document.getElementById('cart-btn');
+
+cart.addEventListener('click', (e)=> {
+    // e.preventDefault();
+
+    // add 
+    // console.log(cart.value);
+    addItemToCart(cart.value);
+    // saveScore(cart.value)
+
+})
+
+// this function add item to the session storage
+function addItemToCart(item) {
+
+    // get the current cart items
+    let cart = JSON.parse(localStorage.getItem('cart')) || {};
+
+    // check if cart has value with item as key
+    if (cart.hasOwnProperty(item)){
+
+        // increament the cart
+        cart[item]++;
+    }else{
+        // asign one to the cart
+        cart[item] = 1;     
+    }
+
+    // set the cart witthe item value
+    localStorage.setItem('cart', JSON.stringify(cart));
+
+    // show the cart
+    // displayCart();
+    displayCart();
+}
+
+    // Return the avaialble cart from the storage
+    function getCartFromStorage() {
+
+        // check if there is item in the session storage
+        if(sessionStorage.length >= 0){
+
+            // convert the item to JSON object 
+            const cart_object = JSON.parse(localStorage.getItem('cart'));
+
+            // return the value
+            return cart_object;
+
+        } else {
+
+            // this code block run if the session storage is empty
+            return '';
+        }
+    }
+
+
+function displayCart(){
+    const cart_display = document.getElementById('cart-item');
+    cart_display.textContent =''
+    const cart = getCartFromStorage();
+    
+    
+
+    // check if the request retunr empty
+    if(cart != null || cart != undefined ){
+        // using the Object key loop through the cart
+        Object.keys(cart).forEach(function(key){
+            // check if the cart object has values
+            if(Object.values(cart) != 0){
+                // console.log(Object.values(cart)[0]);
+                var sum = 0;
+                for (let i = 0; i < Object.values(cart).length; i++){
+                    sum += Object.values(cart)[i];
+                };
+                // console.log(sum)
+                       
+                const cart_display = document.getElementById('cart-item');
+                cart_display.textContent = sum;
+
+            }
+
+        })
+    }
+
+
+}
 
