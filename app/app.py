14d2d32505aef1ce450035ea88d740ada270746a
abel <ab.bell205@gmail.com>
2023-10-16 16:47:10 +0100

    added home for details

--- app/app.py
+++ app/app.py
@@ -10,14 +10,24 @@ def hello():
     data = querry_api()
     return render_template('recipe/index.html',results =data)
 
+@app.route('/home')
+def home():
+    data = querry_api()
+    return render_template('recipe/index.html',results =data)
+
 @app.route('/test')
 def test():
     data = querry_api()
     return render_template('recipe/test.html', results = data)
 
-@app.route('/details')
+@app.route('/details', methods=['POST'])
 def detail():
-    return render_template('recipe/detail.html')
+    # uri = request.form.get('item-details')
+    uri = request.form['item-details']
+    data = query_uri(uri)
+    return render_template('home.html', results = data)
+
+
 
 @app.route('/search',  methods=['POST'])
 def process_data():
@@ -33,7 +43,7 @@ def search_recipe():
     select = request.args.get('select')
     results = querry_api(search, select)
 
-    search_data = ['','Calories','Diets','Ingredients']
+    search_data = ['','Cuisine Type','Diets','Ingredients']
     if select == '0':
         search = search
     else:
@@ -56,7 +66,7 @@ def querry_api(querry_string =None, select_category=None):
     if select_category == '0':
         base_url = 'https://api.edamam.com/api/recipes/v2?type=public&app_id={}&app_key={}&q={}'.format(app_id,app_key,querry_string)  
     elif select_category == '1':
-        base_url = 'https://api.edamam.com/api/recipes/v2?type=public&app_id={}&app_key={}&calories={}'.format(app_id,app_key,querry_string)
+        base_url = 'https://api.edamam.com/api/recipes/v2?type=public&app_id={}&app_key={}&cuisineType={}'.format(app_id,app_key,querry_string)
     elif select_category == '2':
         base_url = 'https://api.edamam.com/api/recipes/v2?type=public&app_id={}&app_key={}&diet={}'.format(app_id,app_key,querry_string)
     elif select_category == '3':
@@ -67,7 +77,7 @@ def querry_api(querry_string =None, select_category=None):
     data = json.loads(resp.text)
     return data
 
-         
+    
 
     # diet = ['high-protein','low-carb','low-fat', 'low-sodium', 'balanced', 'high-fiber']
     # base_url = 'https://api.edamam.com/api/recipes/v2?type=public&app_id={}&app_key={}&imageSize=REGULAR'.format(app_id,app_key)
@@ -75,18 +85,26 @@ def querry_api(querry_string =None, select_category=None):
     # base_url = 'https://api.edamam.com/api/recipes/v2?type=public&app_id={}&app_key={}&ingr=1-2'.format(app_id,app_key)
     # base_url = 'https://api.edamam.com/api/recipes/v2?type=public&app_id={}&app_key={}&diet=low-fat'.format(app_id,app_key)
     # base_url = 'https://api.edamam.com/api/recipes/v2?type=public&app_id={}&app_key={}&calories=100-200'.format(app_id,app_key)
-def api_call():
-    # diet = 'diets'cl
-    app_key  = 'ce7b607fd4979d30ac028afd9897275d'
-    app_id = "142ecb9c"
+
+def query_uri(url):
+    # diet = 'diets'
+    # url="http://www.edamam.com/ontologies/edamam.owl#recipe_bd9abdc18478824909938628dcc574aa"
     
+    app_key  = "ce7b607fd4979d30ac028afd9897275d"
+    app_id = "142ecb9c"
+    # uri= 'https://api.edamam.com/api/recipes/v2/by-uri?type=public&app_id=142ecb9c&app_key=ce7b607fd4979d30ac028afd9897275d&uri={}'.format(url)
+    # url ='https://api.edamam.com/api/recipes/v2/by-uri?type=public&uri={}&app_id=142ecb9c&app_key=ce7b607fd4979d30ac028afd9897275d'.format(uri)
+
     param = {
         'app_id' : app_id,
-        'api_key':app_key
+        'api_key':app_key,
+        'uri':url
     }
-    base_url = 'https://api.edamam.com/api/recipes/v2?type=public&app_id={}&app_key={}&calories=100-200'.format(app_id,app_key)
-    resp = requests.get(base_url, params=param)
+    base_url = "https://api.edamam.com/api/recipes/v2/by-uri?type=public&app_id={}&app_key={}&uri={}".format(app_id,app_key,url)
+    resp = requests.get(base_url,params=param)
+    # print(uri)
     data = json.loads(resp.text)
+    # print(resp)
 
     # print(data)
     return data
