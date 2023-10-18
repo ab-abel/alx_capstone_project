feb03247c1dded89a372d72bf96e3c673eb605f6
abel <ab.bell205@gmail.com>
2023-10-18 11:47:43 +0100

    added doc strings

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,66 +1,24 @@
-// get input element from field
 
-const select = document.getElementById('select-id')
-const alert = document.getElementById('alert')
-
-
-
-select.addEventListener('change', (e)=>{
-    e.preventDefault();
-    document.getElementById('alert').innerHTML = '';
-
-    if(select.value == 1){
-        const span = document.createElement('span');
-        span.setAttribute('class', 'close-alert');
-        span.innerHTML = '&times;';
-       
-        const p = document.createElement('p');
-        p.style.margin = 0;
-        p.innerHTML = 'Keyword Hint: American, Asian, British, Chinese, Central Europe, French, India';
-        alert.appendChild(span);
-        alert.appendChild(p);
-        span.setAttribute('onclick',"this.parentElement.style.display='none'")
-        
-    }
-    else if(select.value == 2){
-        const span = document.createElement('span');
-        span.setAttribute('class', 'close-alert');
-        span.innerHTML = '&times;';
-       
-        const p = document.createElement('p');
-        p.innerHTML = 'Keyword Hint: Balanced, Low-Sodium, Low-Fat, Low-Carb, High-Protein, High-Fiber';
-        alert.appendChild(span);
-        alert.appendChild(p);
-        span.setAttribute('onclick',"this.parentElement.style.display='none'")
-        
-    }
-    else if(select.value == 3) {
-        const span = document.createElement('span');
-        span.setAttribute('class', 'close-alert');
-        span.innerHTML = '&times;';
-        span.setAttribute('onclick',"this.parentElement.style.display='none'")
-        const p = document.createElement('p');
-        p.innerHTML = 'Hint: Enter 1-3 to search ingredients between that range';
-        alert.appendChild(span);
-        alert.appendChild(p);
-    }
-})
-
-// add all item to cart
+// add all item to cart btn in the page
 const cart = document.querySelectorAll('#cart-btn');
 if(cart) {
     cart.forEach(item =>{
         item.addEventListener('click', (e)=> {
-            // e.preventDefault();
-            // console.log(cart.value);
+
+            // add item to car
             addItemToCart(item.value);
-            // saveScore(cart.value)
-        })
-    })
-}
+
+        });
+    });
+};
 
 
-// this function add item to the session storage
+/**
+ * @function addItemToCart
+ * @param {String} item 
+ * @returns None
+ * @description This function add an item to storage
+ */
 function addItemToCart(item) {
 
     // get the current cart items
@@ -84,7 +42,12 @@ function addItemToCart(item) {
     displayCart();
 }
 
-// this function remove an item from storage
+/**
+ * @function removeItemfromCart
+ * @param {String} item 
+ * @returns None
+ * @description This function remove an item from storage
+ */
 function removeItemfromCart(item){
 
     // Get current cart object
@@ -112,7 +75,13 @@ function removeItemfromCart(item){
     displayCart();
 }
 
-// Return the avaialble cart from the storage
+
+/**
+ * @function getCartFromStorage
+ * @param {} None
+ * @returns None
+ * @description Return the avaialble cart from the storage
+ */
 function getCartFromStorage() {
 
     // check if there is item in the session storage
@@ -131,10 +100,18 @@ function getCartFromStorage() {
     }
 }
 
-
+/**
+ * @function displayCart
+ * @param {} None
+ * @returns None
+ * @description Send the number of item in cart to page
+ */
 function displayCart(){
+    // clear the previous cart display element
     const cart_display = document.getElementById('cart-item');
     cart_display.textContent =''
+
+    // get element from stroage
     const cart = getCartFromStorage();
     
     // check if the request retunr empty
@@ -145,19 +122,20 @@ function displayCart(){
             if(Object.values(cart) != 0){
                 // console.log(Object.values(cart)[0]);
                 var sum = 0;
+                // check the total number of item in the storage
                 for (let i = 0; i < Object.values(cart).length; i++){
+
+                    // sum the values
                     sum += Object.values(cart)[i];
                 };
-                // console.log(sum)
-                       
+
+                // send result to display
                 const cart_display = document.getElementById('cart-item');
                 cart_display.textContent = sum;
-
-            }
-
-        })
-    }
-}
+            };
+        });
+    };
+};
 
 // send local storage data to backend
 //  add uri to the cart li
@@ -186,8 +164,15 @@ function send_cart_to_backend(){
     })
 }
 
-// render cart count to cart page
+/**
+ * @function update_cart_from_storage
+ * @param {} None
+ * @returns None
+ * @description render cart count to cart page
+ */
 function update_cart_from_storage(){
+    
+    // create an array from page uri value
     let cart_uri = [];
     const cart_temp = document.querySelectorAll('#cart-uri');
 
@@ -195,19 +180,21 @@ function update_cart_from_storage(){
         cart_uri.push(item.href);
     })
 
+    // create an array from storage value
     const cart_value = [];
     const stored_data = getCartFromStorage();
     for (let [key, value] of Object.entries(stored_data)) {
         cart_value.push(value);
     }
 
+    // loop through the querry
     for (let i = 0; i < cart_uri.length; i++){
         const cart_counter_temp = document.querySelectorAll('.cart-counter');
         cart_counter_temp[i].textContent = cart_value[i];
-        // console.log(typeof(cart_value[i]));
+
         if (Number(cart_value[i]) < 1 || cart_value[i] === undefined){
-            // location.reload();
-            // console.log(typeof(cart_value[i]));
+            
+            // post cart to backend
             data_to_send = getCartFromStorage();
             fetch('/cart', {
                 method:'POST',
@@ -217,33 +204,74 @@ function update_cart_from_storage(){
                 body: JSON.stringify(data_to_send), 
                 
             }).then(response=>{
-                    
+                // goto a new location base on response
                 if(response.redirected){
-                    // console.log(response.url[0])
+                    
                     window.location.href = response.url;
                 }else{
                     response.json();  
-                }
+                };
             });
-        }
-    }
+        };
+    };
 }
 
 
-// decreament cart item caller
+/**
+ * @function Remove cart caller
+ * @param {} None
+ * @returns None
+ * @description This method decreatment the cart value for every item
+ */
 function remove_cart(){
+    // querry all button with id
     const remove_cart_item_btn = document.querySelectorAll('#remove-cart-item-btn');
+
+    // if not empty
     if(remove_cart_item_btn){
+
+        // iterate over and remove item based on button clicked
         remove_cart_item_btn.forEach(item => {
             item.addEventListener('click',(e)=>{
+
+                // prevent btn from submition
                 e.preventDefault();
+
+                // method call
                 removeItemfromCart(item.value);
+
+                // show the number of item left in cart 
                 update_cart_from_storage();
             });
         });
     }
 }
 
+
+/**
+ * @function send_fav_to_backend
+ * @param {} None
+ * @returns None
+ * @description Send favorite item to DB
+ */
+function send_fav_to_backend(value){
+    // send post to db
+    fetch('/browse', {
+        method:'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(value), 
+        
+    }).then(response=>{
+            
+        if(response.redirected){
+            window.location.href = response.url;
+        }else{
+            response.json();  
+        }
+    });
+}
 
 
 const fav_button = document.querySelectorAll('#fav-btn');
@@ -267,25 +295,8 @@ if(fav_button) {
     }
 }
 
-function send_fav_to_backend(value){
 
-    fetch('/browse', {
-        method:'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(value), 
-        
-    }).then(response=>{
-            
-        if(response.redirected){
-            window.location.href = response.url;
-        }else{
-            response.json();  
-        }
-    });
-}
-
+// method calls
 displayCart();
 send_cart_to_backend();
 update_cart_from_storage();
--- app/static/js/validate.js
+++ app/static/js/validate.js
@@ -1,5 +1,53 @@
-const search_btn = document.getElementById('search-btn');
+// get input element from field
 
+const select = document.getElementById('select-id')
+const alert = document.getElementById('alert')
+
+
+
+select.addEventListener('change', (e)=>{
+    e.preventDefault();
+    document.getElementById('alert').innerHTML = '';
+
+    if(select.value == 1){
+        const span = document.createElement('span');
+        span.setAttribute('class', 'close-alert');
+        span.innerHTML = '&times;';
+       
+        const p = document.createElement('p');
+        p.style.margin = 0;
+        p.innerHTML = 'Keyword Hint: American, Asian, British, Chinese, Central Europe, French, India';
+        alert.appendChild(span);
+        alert.appendChild(p);
+        span.setAttribute('onclick',"this.parentElement.style.display='none'")
+        
+    }
+    else if(select.value == 2){
+        const span = document.createElement('span');
+        span.setAttribute('class', 'close-alert');
+        span.innerHTML = '&times;';
+       
+        const p = document.createElement('p');
+        p.innerHTML = 'Keyword Hint: Balanced, Low-Sodium, Low-Fat, Low-Carb, High-Protein, High-Fiber';
+        alert.appendChild(span);
+        alert.appendChild(p);
+        span.setAttribute('onclick',"this.parentElement.style.display='none'")
+        
+    }
+    else if(select.value == 3) {
+        const span = document.createElement('span');
+        span.setAttribute('class', 'close-alert');
+        span.innerHTML = '&times;';
+        span.setAttribute('onclick',"this.parentElement.style.display='none'")
+        const p = document.createElement('p');
+        p.innerHTML = 'Hint: Enter 1-3 to search ingredients between that range';
+        alert.appendChild(span);
+        alert.appendChild(p);
+    }
+})
+
+// search btn validation
+const search_btn = document.getElementById('search-btn');
 
 search_btn.addEventListener('click', (e)=>{
     document.getElementById('alert').innerHTML =''
