4386878d4bbb2c40ac67c4086a65354d2d03012d
abel <ab.bell205@gmail.com>
2023-10-25 01:14:22 +0100

    remove whitespace

--- app/app.py
+++ app/app.py
@@ -12,11 +12,9 @@ app = Flask(os.getenv('APP_NAME'),
             template_folder='app/templates',
             static_folder='app/static')
 
-
 # load routes
 from core.routes import bp as main_bp
 app.register_blueprint(main_bp)
-
 
 # run the code
 if __name__ == '__main__':
